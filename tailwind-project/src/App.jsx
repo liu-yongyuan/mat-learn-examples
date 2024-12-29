8f5b13edc4f0ee86b9181a73eb31aa2fcba48bad
Matt Liu <liuyychn@outlook.com>
2024-12-29 17:29:11 +0800

    abstract color to plugins and apply tailwind css

--- tailwind-project/src/App.jsx
+++ tailwind-project/src/App.jsx
@@ -3,11 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-        <section className='bg-green-400 flex items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aspernatur amet inventore, culpa sapiente quasi aut tenetur deserunt aperiam dolorum repellat optio, nostrum sed expedita autem commodi ex, fugiat maiores.</p>
-        </section>
-      </div>
+      <div className='card rounded-none m-2'>This is a card.</div>
     </>
   )
 }
